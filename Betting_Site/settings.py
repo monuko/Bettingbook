ab8547a4de61ff482d17a88d67c515e6a8f3f9f5
NyleD <nyle.dharani@gmail.com>
2019-05-13 23:12:59 -0400

    white noise

--- Betting_Site/settings.py
+++ Betting_Site/settings.py
@@ -194,7 +194,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Update Database Configuration
 import dj_database_url 
